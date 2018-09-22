7c2a494870d0c60b4611000a9ef3a1ff0168b3ed
villeloh <villealla@gmail.com>
2018-09-22 13:14:42 +0300

    Refactored the Ship class and added ShipManager class, to enable more flexible manipulation of ships. Added Planet class (empty atm) to similarly control Planet-related operations. Moved most constants to their respective, relevant files (Configuration.kt should be removed eventually).

---
+++ app/src/main/java/villealla/com/arinvaders/Planet.kt
@@ -0,0 +1,18 @@
+package villealla.com.arinvaders
+
+/*
+* The planet probably won't have too many stats, but for the sake of consistency,
+* I added it as a class as well.
+* @author Ville Lohkovuori
+* */
+
+class Planet(hitPoints: Long = 7000000000) {
+
+    // we could have a PlanetManager to do this, to mimic the pattern with ships,
+    // but since only one planet is needed, I think that'd be overkill
+    fun renderInArSpace() {
+
+
+    }
+
+} // end class
---
+++ app/src/main/java/villealla/com/arinvaders/ShipManager.kt
@@ -0,0 +1,81 @@
+package villealla.com.arinvaders
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import java.util.*
+
+/*
+* Controls the collective operations regarding Ships
+* (creation as waves, tracking, etc).
+* @author Ville Lohkovuori
+* */
+
+// pass 'this' as the context from MainActivity
+// NOTE: ideally, this class would be a Singleton, but I could think of no good
+// way to refer to the right context and earthNode in that case
+class ShipManager(val context: Context, val earthNode: Node) {
+
+    companion object {
+        const val DEFAULT_SPAWN_DISTANCE_MIN = 1
+        const val DEFAULT_SPAWN_DISTANCE_MAX = 2
+        const val DEFAULT_NUM_OF_SHIPS_IN_WAVE = 15
+    }
+
+    private val shipMap = mutableMapOf<String, Ship>()
+
+    private fun trackShip(shipToTrack: Ship) {
+
+        shipMap[shipToTrack.id] = shipToTrack
+    }
+
+    private fun untrackShip(shipToRemove: Ship) {
+
+        shipMap.remove(shipToRemove.id)
+    }
+
+    fun spawnShip(ship: Ship) {
+
+        val renderable = ModelRenderable.builder()
+                .setSource(context, Uri.parse(ship.type.modelName))
+                .build()
+        renderable.thenAccept {  it ->
+
+            // we'll need to make our own Node subclass for moving the ships...
+            // for now let's just try to create them
+            val shipNode = Node()
+            shipNode.renderable = it
+            shipNode.setParent(earthNode)
+            shipNode.renderable.isShadowCaster = false
+            shipNode.name = ship.id // can be used for destroying ships later
+
+            val spawnCoord = generateRandomCoord()
+            shipNode.localPosition = spawnCoord
+
+            shipMap[ship.id] = ship
+        }
+    } // end spawnShip
+
+    // we could add different spawn patterns (chosen by enum perhaps)
+    fun spawnWaveOfShips(numOfShips: Int, ship: Ship) {
+        for (item in 0..numOfShips) {
+
+            spawnShip(ship)
+        }
+    }
+
+    private fun generateRandomCoord(): Vector3 {
+
+        val randomGen = Random()
+        val x = (if (randomGen.nextBoolean() == true) 1 else -1) * randomGen.nextFloat() * 0.35
+        val y = randomGen.nextFloat() * 0.35
+        val z = (if (randomGen.nextBoolean() == true) 1 else -1) * randomGen.nextFloat() * 0.35
+        Log.d("XYZ", "$x $y $z")
+
+        return Vector3(x.toFloat(), y.toFloat(), z.toFloat())
+    }
+
+} // end class
