98e83e13f6ced9cd7c606bac2f4fba056dc83ab5
villeloh <villealla@gmail.com>
2018-10-03 15:06:25 +0300

    Added 6 new ufo models (I left the old one just in case, even though it's no longer being used). Added 'Thrall' and 'Mothership' class ships in ShipType. Removed the 'Fighter' ship type and model (it clashes with the overall look of the app too much to be usable).

--- app/src/main/java/villealla/com/arinvaders/Static/ShipType.kt
+++ app/src/main/java/villealla/com/arinvaders/Static/ShipType.kt
@@ -9,7 +9,7 @@ const val DEFAULT_MOVE_SPEED = 10
 const val DEFAULT_HP = 1
 
 enum class ShipType(val modelName: String, val speed: Int, val hp: Int, val dmgScaleValue: Float) {
-    UFO("CUPIC_FYINGSAUCER.sfb", DEFAULT_MOVE_SPEED, DEFAULT_HP, 1.0f),
-    FIGHTER("SciFi_Fighter_AK5.sfb", DEFAULT_MOVE_SPEED, DEFAULT_HP, 1.3f)
-    // TODO: add more ships as we get more models
+    UFO("ufo_small_red.sfb", DEFAULT_MOVE_SPEED, DEFAULT_HP, 1.0f),
+    THRALL("ufo_medium_yellow.sfb", DEFAULT_MOVE_SPEED, DEFAULT_HP, 2.0f),
+    MOTHERSHIP("ufo_large_green.sfb", DEFAULT_MOVE_SPEED, DEFAULT_HP, 3.0f)
 }
